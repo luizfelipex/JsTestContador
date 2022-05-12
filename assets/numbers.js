cc216fffb548521de471d2c43444fad3b0f937b0
Luiz Felipe Ferreira <89280707+luizfelipex@users.noreply.github.com>
2022-05-12 14:17:16 -0300

    commit

--- assets/numbers.js
+++ assets/numbers.js
@@ -1,13 +1,17 @@
+// indicação da variável de numero, indicada no documento HTML
 var currentNumber = 0;
 
+// função de adição de incremento, de acordo com o botão de incremento no HTML
 function increment(){
-    updateDysplay(++currentNumber);
+    updateDisplay(++currentNumber);
 }
 
+// função de subtração de incremento, de acordo com o botão de incremento no HTML
 function decrement(){
-    updateDysplay(--currentNumber);
+    updateDisplay(--currentNumber);
 }
 
-function updateDysplay(number) {
+// função de exibição do resultado entre os botões na página HTML 
+function updateDisplay(number) {
     document.getElementById("currentNumber").innerHTML = number;
 }
